89a79231f23010edb7fc8433e499bb90d40fe8a1
Ravi Renz Olayres <renzolayres88@gmail.com>
2018-01-19 09:49:31 +0800

    About Page

---
+++ AndersonNotification/AndersonNotificationWeb/Controllers/NotificationController.cs
@@ -0,0 +1,45 @@
+﻿using AndersonNotificationFunction;
+using AndersonNotificationModel;
+using System;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace AndersonNotificationWeb.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private IFNotification _iFNotification;
+        public NotificationController(IFNotification iFNotification)
+        {
+            _iFNotification = iFNotification;
+        }
+
+        #region Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new Notification());
+        }
+
+        [HttpPost]
+        public ActionResult Create(Notification notification)
+        {
+            var createdNotification = _iFNotification.Create(notification);
+
+            return RedirectToAction("Index");
+        }
+        #endregion
+
+        [HttpGet]
+        public ActionResult Notification()
+        {
+
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AndersonNotification/AndersonNotificationWeb/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace AndersonNotificationWeb
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureCookie(app);
+            HttpConfiguration config = new HttpConfiguration();
+            AreaRegistration.RegisterAllAreas();
+            app.UseWebApi(config);
+        }
+        public void ConfigureCookie(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "ApplicationCookie",
+                LoginPath = new PathString("/Credential/Login")
+            });
+
+
+        }
+    }
+}
