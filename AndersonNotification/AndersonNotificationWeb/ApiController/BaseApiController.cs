4eba3f550a9e8d9d2554b573c7c4e4d16abb441d
Cris Dismaya <dismaya.marcelino20@gmail.com>
2018-01-26 13:38:56 +0800

    ApiController

---
+++ AndersonNotification/AndersonNotificationWeb/ApiController/BaseApiController.cs
@@ -0,0 +1,10 @@
+﻿using ExternalAccountWebAuthentication.ApiController;
+using ExternalAccountWebAuthentication.Authentication;
+
+namespace AccountExternalWeb.ApiControllers
+{
+    [ApiAuthorizationFilter(false,new string[0])]
+    public class BaseApiController : ExternalAccountBaseApiController
+    {
+    }
+}
